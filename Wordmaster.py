3045afba7e002eed41efb14c47aa577070fd09d1
ginmanga <paboenhj@outlook.com>
2018-03-03 16:23:45 +0800

    "glob to compile paths"

--- Wordmaster.py
+++ Wordmaster.py
@@ -4,8 +4,19 @@
 import os
 
 path_1 = "C:/Users/Panqiao/Documents/Research/AICPA/Files to separate/NO GVKEY"
-print(os.listdir(path_1))
-lpath_l = os.listdir(path_1)
-for i in lpath_l:
-    print(i)
+#print(os.listdir(path_1))
+#lpath_l = os.listdir(path_1)
+#for i in lpath_l:
+    #print(i)
 
+
+
+tp = "C:/Users/Panqiao/Documents/Research/AICPA/Files to separate/NO GVKEY/Annual_Reports_-_Corporate_(AICPA)__1972-1982011-05-07_23-05.doc"
+
+fhand = open(tp)
+i = 0
+for line in fhand:
+    print(line)
+    i+=1
+    if i > 50:
+        break
---
+++ convdoc.py
@@ -0,0 +1,7 @@
+"""Script to Convert old word files into .docx files"""
+import glob
+
+path_1 = "C:/Users/Panqiao/Documents/Research/AICPA/Files to separate/NO GVKEY"
+
+path = glob.glob('C:\\Users\\Panqiao\\Documents\\Research\\AICPA/Files to separate\\NO GVKEY\\*.doc', recursive=False)
+print(path)
