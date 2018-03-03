ae851b07f7fd73556b6d027b4f92d02f4c6be2bb
ginmanga <paboenhj@outlook.com>
2018-03-03 15:03:25 +0800

    "test"

---
+++ Wordmaster.py
@@ -0,0 +1,10 @@
+""" Script to read AICPA Word Files"""
+# Parse word files and separate them into individual documents
+# Save individual files as txt for data gathering
+import os
+
+path_1 = "C:/Users/Panqiao/Documents/Research/AICPA/Files to separate/NO GVKEY"
+print(os.listdir(path_1))
+lpath_l = os.listdir(path_1)
+for i in lpath_l:
+    print(i)
