1a5fc7d4d23f5dc73cfec6876546b5728e0283ef
ginmanga <paboenhj@outlook.com>
2018-03-03 16:56:35 +0800

    "glob to compile paths"

--- convdoc.py
+++ convdoc.py
@@ -1,7 +1,19 @@
 """Script to Convert old word files into .docx files"""
 import glob
+import win32com.client
+import os
 
-path_1 = "C:/Users/Panqiao/Documents/Research/AICPA/Files to separate/NO GVKEY"
+word = win32com.client.Dispatch("Word.Application")
+word.visible = 0
 
-path = glob.glob('C:\\Users\\Panqiao\\Documents\\Research\\AICPA/Files to separate\\NO GVKEY\\*.doc', recursive=False)
-print(path)
+path = glob.glob('C:\\Users\\Panqiao\\Documents\\Research\\AICPA\\Files to separate\\NO GVKEY\\*.doc', recursive=False)
+#print(path)
+
+for i in path:
+    in_file = os.path.abspath(i)
+    wb = word.Documents.Open(in_file)
+    out_file = os.path.abspath("out{}.docx".format(i))
+    wb.SaveAs2(out_file, FileFormat=16) # file format for docx
+    wb.Close()
+
+word.Quit()
