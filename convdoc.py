5188b9e41f11be884e6cb700f6c66987ac39b3a4
ginmanga <paboenhj@outlook.com>
2018-03-14 14:33:41 +0800

    "User input and glob path working, need to clean"

--- convdoc.py
+++ convdoc.py
@@ -5,44 +5,26 @@ import os
 
 word = win32com.client.Dispatch("Word.Application")
 word.visible = 0
-#def ask_path(x):
-x = r'C:\Users\Panqiao\Documents\Research\AICPA\Files to separate\GVKEY'
+#x = r'C:\Users\Panqiao\Documents\Research\AICPA\Files to separate\GVKEY'
 #x.replace('\','\\')
 #print(os.path.abspath(x))
 #x = os.path.abspath(x)+r'\*.doc'
 #print(x)
 #path1 = glob.glob(x, recursive=False)
-#y='C:\\Users\\Panqiao\\Documents\\Research\\AICPA\\Files to separate\\NO GVKEY\\*.doc'
-#print(y)
 
-#def usr_path():
-    #path = input('Path to files to convert:\n')
-    #xx = 'r'%path
-    #return xx
-
-#def usr_filename():
-    #n = input('Type:\n')
-    #yy = 'r'%n #"%r"%n
-    #return yy
 
 path = input('Path to files to convert:\n')
 file_type = input('Type:\n')
-#print(usr_path())
-#pathn = usr_path()
-#filee = usr_filename()
-print(path)
-print(file_type)
+
 y = os.path.join(path,file_type)
-#y = pathn
-print(y)
-print("here")
+
 #path = glob.glob('C:\\Users\\Panqiao\\Documents\\Research\\AICPA\\Files to separate\\GVKEY\\**/*.doc', recursive=False)
 #s = r'C:\\Users\\Panqiao\\Documents\\Research\\AICPA\\Files to separate\\GVKEY\\**/*.doc'
 #print(s)
 path = glob.glob(y, recursive = True)
-print(path)
-#for i in path:
-    #print(i)
+
+for i in path:
+    print(i)
 
 
 for i in path:
