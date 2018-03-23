38e40d92aaf6fdffb55f859b2b4e670f3501bb33
ginmanga <paboenhj@outlook.com>
2018-03-23 09:51:17 +0800

    "Use DOCX to parse files: identifying start of document"

--- Wordmaster.py
+++ Wordmaster.py
@@ -6,15 +6,14 @@ import docx
 file_experiment = r'C:\Users\Panqiao\Documents\Research\AICPA\Files to separate\Annual_Reports_-_Corporate_(AICPA)__1972-1982011-05-07_23-05.docx'
 file_experiment = os.path.abspath(file_experiment)
 file_test = docx.Document(file_experiment)
-sections = file_test.sections
+#sections = file_test.sections
 paras = file_test.paragraphs
 
-#print(paras[9].text)
+directory_a = r'C:\Users\Panqiao\Documents\Research\AICPA\Files to separate\NO GVKEY'
+#print(os.listdir(directory_a))
 
-#print(paras[9].text[0])
-
-count = 0
-
+print(paras[9].text)
+print(any(char.isdigit() for char in paras[9].text))
 
 def getText(filename):
     doc = docx.Document(filename)
@@ -37,17 +36,53 @@ for i in paras:
 
 los = ['of', 'DOCUMENTS']
 
-#print(terms[0] in paras[9].text)
 def fnd(paragraphs, terms):
     """Given a string of characters find paragraph numbers of each case"""
     #For AICPA files, look for number of number DOCUMENT
+    count_par = 0
+    count_doc = 0
+    list_paras = []
     for i in paragraphs:
         fc = terms[0] in i.text
         sc = terms[1] in i.text
-        if fc == True and sc == True:
-            print(i.text)
+        dc = any(char.isdigit() for char in i.text)
+        c_list = [fc, sc, dc]
 
-fnd(paras, los)
+        #if fc == True and sc == True:
+        if all(cond == True for cond in c_list):
+            #print(i.text)
+            #print(count_par)
+            list_paras.append(count_par)
+            count_doc += 1
+            #print(count_doc)
+        count_par += 1
+    return count_doc, list_paras
+
+a = fnd(paras, los)
+
+#b = [0 , 0 ,0]
+#bb = [0 ,1, 0]
+
+#print(all(i == 0 for i in bb))
+#print(a[0])
+#print(a[1])
+
+directory = r'C:\Users\Panqiao\Documents\Research\AICPA\Files to separate\NO GVKEY'
+#print(os.listdir(directory_a))
+los = ['of', 'DOCUMENTS']
+
+
+
+def parse_AICPA(gvkey, path):
+    for file in os.listdir(path):
+        file_path = os.path.join(directory_a, file)
+        print(file)
+        file_doc = docx.Document(file_path)
+        paras = file_doc.paragraphs
+        a = fnd(paras, los)
+        print(a[0])
+
+parse_AICPA(0, directory)
 
 #for i in file_test.sections:
     #if i.text != '' and i.text.isspace() == False:
