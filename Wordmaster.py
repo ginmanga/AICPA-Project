d7139ded485ba0fa3042e79ecc6db0f5e09b985c
ginmanga <paboenhj@outlook.com>
2018-03-23 08:15:54 +0800

    "Use DOCX to parse files: identifying start of document"

--- Wordmaster.py
+++ Wordmaster.py
@@ -9,6 +9,10 @@ file_test = docx.Document(file_experiment)
 sections = file_test.sections
 paras = file_test.paragraphs
 
+#print(paras[9].text)
+
+#print(paras[9].text[0])
+
 count = 0
 
 
@@ -21,13 +25,30 @@ def getText(filename):
 #file_text = getText(file_experiment)
 #print(file_text[0:500])
 count = 0
-for i in file_test.paragraphs:
+#for i in file_test.paragraphs:
+for i in paras:
     if i.text != '' and i.text.isspace() == False:
-        print(i.text)
-        print(count)
+        None
+        #print(i.text)
+        #print(count)
     if count>200:
         break
     count += 1
+
+los = ['of', 'DOCUMENTS']
+
+#print(terms[0] in paras[9].text)
+def fnd(paragraphs, terms):
+    """Given a string of characters find paragraph numbers of each case"""
+    #For AICPA files, look for number of number DOCUMENT
+    for i in paragraphs:
+        fc = terms[0] in i.text
+        sc = terms[1] in i.text
+        if fc == True and sc == True:
+            print(i.text)
+
+fnd(paras, los)
+
 #for i in file_test.sections:
     #if i.text != '' and i.text.isspace() == False:
         #print(i.text)
