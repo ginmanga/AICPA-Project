3b9a301aa0cfb01a094c1791fd1567bc4b1eed4c
ginmanga <paboenhj@outlook.com>
2018-03-24 14:37:09 +0800

    "Use to extract text based on paragraph numbers"

--- Separate - docx.py
+++ Separate - docx.py
@@ -1,1 +1,5 @@
-"""Separate AICPA DOCX Files That Contain Multiple Documents"""
+"""Gather Text and Data from AICPA files using Wordmaster as input"""
+from Wordmaster import fipath
+directory = r'C:\Users\Panqiao\Documents\Research\AICPA\Files to separate\Annual_Reports_-_Corporate_(AICPA)__1972-1982011-05-07_23-05.docx'
+
+print(fipath(0, directory))
