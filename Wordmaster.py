153595d3571b00a312dd5447a89bb403a08e2095
ginmanga <paboenhj@outlook.com>
2018-03-25 13:11:50 +0800

    "Changing fipath"

--- Wordmaster.py
+++ Wordmaster.py
@@ -3,12 +3,17 @@
 import os
 import docx
 import glob
-def getText(filename):
+
+def getText(filename, a):
     """Function gathers text fom docx file"""
     #Will add a variable that takes the list of paragraph numbers within the file
     #And looks for the needed text to gather data
     doc = docx.Document(filename)
     fullText = []
+    b = a[2][0]
+    print("GETTEXT")
+    print(filename)
+    print(doc.paragraphs[b].text)
     for para in doc.paragraphs:
         fullText.append(para.text)
     return '\n'.join(fullText)
@@ -30,32 +35,39 @@ def fnd(paragraphs, terms):
         count_par += 1
     return count_doc, list_paras
 
-#directory = r'C:\Users\Panqiao\Documents\Research\AICPA\Files to separate\Annual_Reports_-_Corporate_(AICPA)__1972-1982011-05-07_23-05.docx'
-#directory = r'C:\Users\Panqiao\Documents\Research\AICPA\Files to separate\GVKEY'
-#directory = r'C:\Users\Panqiao\Documents\Research\AICPA\Files to separate\NO GVKEY'
-#directory = r'C:\Users\Panqiao\Documents\Research\AICPA\Files to separate\Annual_Reports_-_Corporate_(AICPA)__1972-1982011-05-07_23-05.docx'
-los = ['of', 'DOCUMENTS']
-
 def fsttotal(file_path, file_name):
     """Function to find start and total documents"""
+    los = ['of', 'DOCUMENTS']
     a = [file_name]
-    print(a)
+    print("FSTTTTTTTOOOTTAkllll")
+    print("File name %s" % (a))
+    print("File path  %s" %(file_path))
     file_doc = docx.Document(file_path)
     #print("MAAAADDDEE")
     paras = file_doc.paragraphs
     a.extend(fnd(paras, los))
     return a
 
+def file_loop(path):
+
 def fipath(gvkey, path):
     """Function delivers path to files to open"""
     path = os.path.abspath(path)
-    try:
+    print(os.path.isdir(path))
+    if os.path.abspath(path) == False:
         file_name = os.path.splitext(os.path.basename(path))[0]
         # get file name without path or extension
         a = fsttotal(path, file_name)
         return a
-    except:
-        None
+
+    #try:
+        #print("HERE?")
+        #file_name = os.path.splitext(os.path.basename(path))[0]
+        # get file name without path or extension
+        #a = fsttotal(path, file_name)
+        #return a
+    #except:
+        #None
     for file in os.listdir(path):
         #Loops through files and folders in path
         #calls fsttotal function
@@ -63,9 +75,11 @@ def fipath(gvkey, path):
         if os.path.isdir(file_path_a) == True:
             for i in os.listdir(file_path_a):
                 file_path_open = os.path.join(file_path_a, i)
+                print("FIPATH path %s" % (file_path_open))
                 a = fsttotal(file_path_open, os.path.splitext(i)[0])
+                getText(file_path_open, a)
+                print(a)
         else:
             a = fsttotal(file_path_a, os.path.splitext(file)[0])
+            print(a)
     return a
-
-#print(fipath(0, directory))
